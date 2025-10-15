5aed4040565c908efa2770d60cf028a282cd8759
jph-sw <jph.sw@proton.me>
2025-10-15 18:19:55 +0000

    feat: replace favicon

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -31,9 +31,8 @@ export const Route = createRootRouteWithContext<{
         content: "width=device-width, initial-scale=1",
       },
       ...seo({
-        title:
-          "TanStack Start | Type-Safe, Client-First, Full-Stack React Framework",
-        description: `TanStack Start is a type-safe, client-first, full-stack React framework. `,
+        title: "yan",
+        description: `yet another note `,
       }),
     ],
     links: [
